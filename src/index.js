50b6cec7d3920d6ce21e69fe6accbd3f35610100
Aleksan Vartapetyan <alex.240941.123@gmail.com>
2024-12-09 18:10:26 +0900

    project init

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const pino = require("pino-http");
+const stripe = require("stripe")(
+  "sk_test_51QH1RuAGiLDyLsr1ht1TxBc3rUb483621kVYgKO2he4C75W6jZdFrr2DwjRFoGdN85fhboRyX636gHHPiNbr14yf001GoOfBqp"
+);
+const app = express();
+
+app.use(express.json());
+app.use(pino());
+app.use(cors());
+
+app.post("/checkout", async (req, res) => {
+  console.log(req.body);
+  try {
+    const session = await stripe.checkout.sessions.create({
+      line_items: req.body,
+      mode: "payment",
+      success_url: "http://localhost:3000/success",
+      cancel_url: "http://localhost:3000/cancel",
+    });
+    res.json({ checkoutUrl: session.url, sessionId: session.id });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// app.get("/checkout", async (req, res) => {
+//   try {
+//     const session = await stripe.checkout.sessions.retrieve(req.body);
+//     console.log(session);
+//   } catch (error) {
+//     console.log(err);
+//   }
+// });
+
+app.listen(8000, () => console.log("server runnig on 8000 port"));
